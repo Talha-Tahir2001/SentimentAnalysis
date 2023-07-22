204e34744d54a58cd007d9da70ed0e0656241827
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2023-07-22 15:11:15 +0500

    Add files via upload

---
+++ ML Demo/ML Demo/Program.cs
@@ -0,0 +1,14 @@
+﻿using ML_Demo;
+
+// Add input data
+var sampleData = new SentimentModel.ModelInput()
+{
+    Col0 = "Fuck you."
+};
+
+// Load model and predict output of sample data
+var result = SentimentModel.Predict(sampleData);
+
+// If Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative"
+var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
+Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
